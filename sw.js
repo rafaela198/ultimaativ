4345e7931393993eb8865d271f2b66dba34261d2
rafaela198 <rafaela.souza2@estudante.ifms.edu.br>
2020-11-30 16:29:28 -0400

    cache

--- sw.js
+++ sw.js
@@ -1,5 +1,23 @@
+const cacheName = "Tecnoticias"
+const files = [
+  '/ultimaativ/',
+  '/ultimaativ/index.html'
+  '/ultimaativ/style.css'
+  'https://www.techtudo.com.br/noticias/2020/05/nokia-festeja-155-anos-relembre-os-celulares-mais-marcantes.ghtml',
+  'https://www.nsctotal.com.br/noticias/a-tecnologia-durante-tempos-de-pandemia',
+  'https://canaltech.com.br/apps/app-google-modo-escuro-android-ios-165079/',
+  'https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css'
+]
+
 self.addEventListener('install', function(evt) {
-  console.log("install sw");
+
+  evt.waitUntil(
+    caches.open(cacheName).then(function (cache) {
+    console.log('colocando arquivos na cache');
+    cache.addAll(files)
+  })
+  )
+
 })
 
 self.addEventListener('activate', function(evt) {
